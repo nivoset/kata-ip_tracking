ccab9af2f0e31c342d0d6a44b6aac10450d76e37
Ben Koop <nivoset@gmail.com>
2021-10-31 07:41:03 -0400

    cleaned up code to be more in line

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -27,13 +27,12 @@ it('should give top 100 results', () => {
   expect(tracker.top100()).toMatchSnapshot();
   // generate 10000 pseudo-random requests and check if top 100 is correct
   // the "ip addresses" used are just strings of data showing index as well as number of 'calls'
-  const requests = new Array(700).fill(0)
+  new Array(700).fill(0)
       .map(() => Math.floor(rand() * 10000))
-      .map((r, i) => [`ip index=${i} numberOfRequests=${r}`, r]);
-
-  requests.map(([ip, count]: any) => new Array(count).fill(ip))
-    .flat()
-    .forEach(tracker.request_handled);
+      .map((r, i) => [`ip index=${i} numberOfRequests=${r}`, r])
+      .map(([ip, count]: any) => new Array(count).fill(ip))
+      .flat()
+      .forEach(tracker.request_handled);
 
   // only used a snapshot here as the order of the top 100 is a large set of data
   expect(tracker.top100()).toMatchSnapshot();
